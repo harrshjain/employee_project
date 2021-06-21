a7d47065d4245c4a9b63139fca917ba97df8d37e
Harsh Jain <harshjacksn@gmail.com>
2021-06-21 20:23:28 +0530

    Update settings.py

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'EmployeeDB',
         "USER": 'postgres',
-        "PASSWORD": 'surakshit',
+        "PASSWORD": 'postgres',
         "HOST": "localhost",
     }
 }
